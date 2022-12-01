788df510180f08ca1a66a406d4d8b01e1d0dea08
FarisRehmani <20020183@stu.mmu.ac.uk>
2022-12-01 12:54:32 +0000

    Put map next to instagram embed on index page

--- js/scripts.js
+++ js/scripts.js
@@ -117,6 +117,7 @@ $(function() {
         });
     };
 
+
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
